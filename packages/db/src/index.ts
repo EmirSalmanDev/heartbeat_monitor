bc4858586ab82aaaf020fe18bb330d2df4674815
Emir Salman <emirsalman.info@gmail.com>
2026-03-21 19:04:22 +0300

    feat(infra): add Prisma schema and DB setup

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+export { PrismaClient, Prisma } from "../generated/client/index.js";
+export type {
+  User,
+  Monitor,
+  Check,
+  Alert,
+  MonitorStatus,
+  CheckResult,
+  AlertType,
+} from "../generated/client/index.js";
